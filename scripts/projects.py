9432eb9edd6e981a46b4227e4eaf27ab7381ec10
Yifeng He <yfhe@ucdavis.edu>
2023-02-16 17:02:42 -0800

    migrate all build-fuzz process to oss-fuzz

--- scripts/projects.py
+++ scripts/projects.py
@@ -6,100 +6,65 @@ from logging import error, info, warning
 from functools import partial
 import argparse
 import warnings
-from util import fuzz_one_instance
+from util import oss_fuzz_one_target
 
 
 class Project:
-    seeds_dir: str = ""
-    target: str = ""
+    def __init__(self, project: str, fuzzdir: str):
+        self.project = project
+        self.fuzzdir = path.join(DETECTION_HOME, fuzzdir)
+        self.targets = []
 
-    def __init__(self):
-        pass
+    def build(self):
+        os.system(f"python3 {OSSFUZZ}/infra/helper.py build_fuzzers {self.project}")
+        self.targets = self._update_targets()
 
-    def get_seeds(self):
-        pass
+    def mkdir_if_doesnt_exist(self):
+        if not path.isdir(self.fuzzdir):
+            os.makedirs(self.fuzzdir)
 
-    def fuzz(self, target: str, jobs=CORES, timeout=3600):
-        if not path.isdir("fuzz"):
-            os.mkdir("fuzz")
-        out = "fuzz"
-        to_fuzz = [(self.target, out, "fuzzer0", "-M")]
-        to_fuzz += [(self.target, out, f"fuzzer{i}", "-S") for i in range(1, jobs)]
-        print(len(to_fuzz))
-        print(timeout)
-        subprocess.run(["sleep", "0.5"])
+        proj_fuzzout = path.join(self.fuzzdir, self.project)
+        if not path.isdir(proj_fuzzout):
+            os.makedirs(proj_fuzzout)
+
+        for t in self.targets:
+            subdir = path.join(proj_fuzzout, str(t))
+            if not path.isdir(subdir):
+                os.makedirs(subdir)
+
+    def _update_targets(self) -> List[str]:
+        bindir = path.join(OSSFUZZ, "build/out", self.project)
+        if not path.isdir(bindir):
+            warning("project build dir doesn't exist, please build the project first")
+        for f in os.listdir(bindir):
+            if os.access(path.join(bindir, f), os.X_OK) and "fuzz" in f:
+                self.targets.append(f)
+
+    def fuzz(self, jobs=CORES, fuzztime=3600):
+        self._update_targets()
+        if not self.targets:
+            warning("no targets found, please build fuzzers first")
+
+        self.mkdir_if_doesnt_exist()
+
+        info("Collecting binaries to fuzz")
+        bins_to_fuzz = [
+            (t, path.join(self.fuzzdir, self.project, t)) for t in self.targets
+        ]
+
+        info(f"Fuzzing all {len(bins_to_fuzz)} binaries for {fuzztime} seconds")
         parallel_subprocess(
-            to_fuzz,
+            bins_to_fuzz,
             jobs,
-            lambda r: fuzz_one_instance(r, timeout=timeout, seeds=self.seeds),
-            use_tqdm=False,
+            lambda r: oss_fuzz_one_target(r, proj=self.project, fuzztime=fuzztime),
+            on_exit=None,
         )
+
+    def postprocess(self):
+        pass
 
     def summarize(self):
         pass
-
-
-class FFmpeg(Project):
-    def __init__(self, seeds_dir):
-        Project.__init__(self)
-        self.seeds = path.join(seeds_dir, "ffmpeg")
-        self.target = f"{FFMPEG}/ffmpeg"
-
-    def get_seeds(self, jobs: int = CORES):
-        # http://samples.ffmpeg.org/
-        domain = "samples.ffmpeg.org"
-        seeds = ["A-codecs", "AVS"]
-        for s in tqdm(seeds):
-            subprocess.run(["wget", "-r", "--no-parent", f"https://{domain}/{s}"])
-        os.system(
-            f"{AFL}/afl-cmin -i {domain} -o minffmpeg -- {self.target} -i @@ ffmpeg"
-        )
-        os.system(f"rm {domain}; mv minffmpeg {self.seeds}")
-
-    def fuzz(self, target: str = "ffmpeg", jobs=CORES, timeout=3600):
-        Project.fuzz(self, target, jobs=jobs, timeout=timeout)
-
-
-class Qemu(Project):
-    def __init__(self, seeds_dir="seeds"):
-        Project.__init__(self)
-        self.seeds = path.join(seeds_dir, "qemu")
-        self.target = f"{QEMU}/build/qemu-x86_64"
-
-    def get_seeds(self):
-        if not os.path.isdir(self.seeds):
-            info(f"make new directory at {self.seeds}")
-            os.mkdir(self.seeds)
-        os.system(
-            f"{AFL}/afl-cmin -i {QEMU}/tests/ -o minqemu -- {self.target} -i @@ qemu"
-        )
-        os.system(f"mv minqemu {self.seeds}")
-
-    def get_fuzz_target(self, target):
-        target_split_names = target.split("-")
-        target_split_names.insert(1, "fuzz")
-        fuzz_target = "-".join(target_split_names)
-        return fuzz_target
-
-    def build_target(self, target: str, jobs=CORES):
-        fuzz_target = self.get_fuzz_target(target)
-        os.system(f"cd {QEMU}/build && make -j{jobs} {target} {fuzz_target}")
-
-    def fuzz(
-        self,
-        target: str = "qemu-x86_64",
-        jobs=CORES,
-        timeout=3600,
-    ):
-        if not path.isdir("fuzz"):
-            os.mkdir("fuzz")
-        fuzz_dir = "fuzz/qemu"
-        if not path.isdir(fuzz_dir):
-            os.mkdir(fuzz_dir)
-        fuzz_target = self.get_fuzz_target(target)
-        os.system(
-            f"timeout {timeout} {QEMU}/build/{fuzz_target} --fuzz-target=generic-fuzz-parallel {fuzz_dir} -jobs={jobs}"
-        )
 
 
 def main():
@@ -108,9 +73,16 @@ def main():
         "-d",
         "--dataset",
         type=str,
-        choices=["FFmpeg", "qemu"],
+        choices=["ffmpeg", "qemu"],
         required=True,
-        help="The dataset to compile",
+        help="The dataset to build",
+    )
+    parser.add_argument(
+        "-o",
+        "--fuzzout",
+        type=str,
+        help="directory to store corpus for the fuzz target",
+        default="fuzz",
     )
     parser.add_argument(
         "-j", "--jobs", type=int, help="Number of threads to use.", default=CORES
@@ -123,26 +95,23 @@ def main():
         default="all",
         choices=[
             "all",
-            "get_seeds",
             "build",
             "fuzz",
+            "postprocess",
             "summarize",
         ],
     )
     parser.add_argument(
-        "-ft", "--fuzztime", type=str, help="Time to fuzz one program", default="1d"
+        "-ft", "--fuzztime", type=str, help="Time to fuzz one program", default="1h"
     )
-    parser.add_argument("-t", "--target", type=str, help="Target to build")
 
     args = parser.parse_args()
 
-    dataset = None
-    if args.dataset == "FFmpeg":
-        dataset = FFmpeg(seeds_dir="seeds")
-    elif args.dataset == "qemu":
-        dataset = Qemu(seeds_dir="seeds")
-    else:
+    aviliable_porjects = os.listdir(path.join(OSSFUZZ, "projects"))
+    if args.dataset not in aviliable_porjects:
         unreachable("No dataset provided.")
+
+    dataset = Project(args.dataset, args.fuzzout)
 
     def convert_to_seconds(s: str) -> int:
         seconds_per_unit = {"s": 1, "m": 60, "h": 3600, "d": 86400, "w": 604800}
@@ -151,15 +120,12 @@ def main():
     if args.pipeline == "all":
         dataset.get_seeds()
         dataset.fuzz(jobs=args.jobs, timeout=convert_to_seconds(args.fuzztime))
-    elif args.pipeline == "get_seeds":
-        dataset.get_seeds()
-    elif args.pipeline == "build" and args.dataset == "qemu":
-        dataset.build_target(args.target, jobs=args.jobs)
+    elif args.pipeline == "build":
+        dataset.build()
     elif args.pipeline == "fuzz":
         dataset.fuzz(
-            target=args.target,
             jobs=args.jobs,
-            timeout=convert_to_seconds(args.fuzztime),
+            fuzztime=convert_to_seconds(args.fuzztime),
         )
     else:
         unreachable("Unkown pipeline provided")
--- scripts/util.py
+++ scripts/util.py
@@ -34,3 +34,24 @@ def fuzz_one_instance(
     # Sleep half a second for AFL to bind core.
     subprocess.run(["sleep", "0.5"])
     return process
+
+
+def oss_fuzz_one_target(p, proj, fuzztime):
+    target, fuzzout = p
+    "python infra/helper.py run_fuzzer qemu qemu-fuzz-i386-target-generic-fuzz-virtio-gpu  qemu-seeds"
+    return subprocess.Popen(
+        [
+            "python3",
+            f"{OSSFUZZ}/infra/helper.py",
+            "run_fuzzer",
+            proj,
+            target,
+            f"\-max_total_time={fuzztime}",
+            "--engine",
+            "libfuzzer",
+            "--corpus-dir",
+            fuzzout,
+        ],
+        stdout=subprocess.DEVNULL,
+        stderr=subprocess.DEVNULL,
+    )
