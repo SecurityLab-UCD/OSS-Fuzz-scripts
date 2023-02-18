494c3cd713e4e9d551139b3846f228b0b3a215b2
Yifeng He <yfhe@ucdavis.edu>
2023-02-17 16:41:17 -0800

    rm AFL++ in script

--- scripts/env.sh
+++ scripts/env.sh
@@ -1,13 +1,7 @@
 #!/bin/bash
 # Environment setup for non-docker user.
 export DETECTION_HOME=`pwd`
-export AFL=$DETECTION_HOME/AFLplusplus
 export LLVM=$HOME/clang+llvm
-export PATH=$PATH:$LLVM:$AFL
-export AFL_NO_UI=1
-unset AFL_CUSTOM_MUTATOR_LIBRARY
-unset AFL_CUSTOM_MUTATOR_ONLY
-export AFL_LLVM_INSTRUMENT=unset PCGUARD 
+export PATH=$PATH:$LLVM
 export CORES=`nproc`
-unset AFL_EXIT_ON_SEED_ISSUES
 export OSSFUZZ=$DETECTION_HOME/oss-fuzz
