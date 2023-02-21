50912bb3f1c6d0842cfb8e4526dd0f6589ea99d4
Yifeng He <yfhe@ucdavis.edu>
2023-02-21 13:15:47 -0800

    chore: rm AFL++ fuzz function

--- scripts/util.py
+++ scripts/util.py
@@ -5,37 +5,6 @@ from logging import error
 from common import *
 
 
-def fuzz_one_instance(
-    p: Tuple[str, str, str, bool, str],
-    timeout: int,
-    seeds: str,
-):
-    binary, out, instance, node_type = p
-    cmd = [
-        f"{AFL}/afl-fuzz",
-        "-D",
-        "-V",
-        str(timeout),
-        node_type,
-        instance,
-        "-i",
-        seeds,
-        "-o",
-        out,
-        binary,
-        "-i",
-        "@@",
-    ]
-    process = subprocess.Popen(
-        cmd,
-        stdout=subprocess.DEVNULL,
-        stderr=subprocess.DEVNULL,
-    )
-    # Sleep half a second for AFL to bind core.
-    subprocess.run(["sleep", "0.5"])
-    return process
-
-
 def oss_fuzz_one_target(p, proj, fuzztime):
     target, fuzzout = p
     "python infra/helper.py run_fuzzer qemu qemu-fuzz-i386-target-generic-fuzz-virtio-gpu  qemu-seeds"
