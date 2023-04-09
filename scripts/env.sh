a6b41e7ae6e3bea976c4c70b1c6fc68e3638bf93
Ethan He <yfhe@ucdavis.edu>
2023-04-09 21:58:16 +0000

    feat: build project with pass

--- scripts/env.sh
+++ scripts/env.sh
@@ -5,8 +5,3 @@ export LLVM=$HOME/clang+llvm
 export PATH=$PATH:$LLVM
 export CORES=`nproc`
 export OSSFUZZ=$DETECTION_HOME/oss-fuzz
-
-export REPORT_PASS=$DETECTION_HOME/ReportFunctionExecutedPass
-export REPORT_FLAGS="-Xclang -load -Xclang $REPORT_PASS/report/libReportPass.so -flegacy-pass-manager"
-export CFLAGS="$CFLAGS $REPORT_PASS/reporter.o $REPORT_FLAGS"
-export CXXFLAGS="$CXXFLAGS $REPORT_PASS/reporter.o $REPORT_FLAGS"
