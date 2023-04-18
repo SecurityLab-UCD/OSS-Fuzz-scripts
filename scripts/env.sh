ce2dc587a68d3af73d879c28156aa868d59aa021
Peter Rong <PeterRong96@gmail.com>
2023-04-17 17:15:14 -0700

    add LLVM/bin to PATH instead of LLVM
    Signed-off-by: Peter Rong <PeterRong96@gmail.com>

--- scripts/env.sh
+++ scripts/env.sh
@@ -2,6 +2,6 @@
 # Environment setup for non-docker user.
 export OSSFUZZ_SCRIPTS_HOME=`pwd`
 export LLVM=$HOME/clang+llvm
-export PATH=$PATH:$LLVM
+export PATH=$PATH:$LLVM/bin
 export CORES=`nproc`
 export OSSFUZZ=$OSSFUZZ_SCRIPTS_HOME/oss-fuzz
