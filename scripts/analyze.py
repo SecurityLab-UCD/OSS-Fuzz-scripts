65fdcb29e3f9ce741c0270396f676c35d3d74432
Ethan Wang <ebwang@ucdavis.edu>
2023-06-06 11:09:06 -0700

    Changes to regex parsing

--- scripts/analyze.py
+++ scripts/analyze.py
@@ -14,7 +14,7 @@ def get_project_fuzzer_stats(project: str) -> pd.DataFrame:
     proj = Project(project, fuzzout, dumpout)
     return proj.get_project_stats()
 
-def get_proj_name(line: str) -> Optional[str]:
+def get_proj_info(line: str) -> Optional[Tuple[str, str]]:
     #pattern = '^[0-9]+\. \[?([A-Za-z0-9\-\_]+)\]?\([A-Za-z\.\_\-/ +]+\),([A-Za-z+]+)'
     # Group 1 is project name, group 2 is project language
     pattern = '^[0-9]+\. \[?([^\]]+)\]?\(.+\),([A-Za-z+]+)'
@@ -27,15 +27,14 @@ def get_proj_name(line: str) -> Optional[str]:
     
     proj_name = match.group(1)
     proj_language = match.group(2)
-    return proj_name
+    return proj_name, proj_language
 
 def main():
     
     with open("./built_w_pass.md", "r") as f:
         lines = f.readlines()
-    projects = list(filter(lambda x: x is not None, map(get_proj_name, lines))) 
-    #projects = ["clib", "coturn", "croaring", "gpac", "h3", "inchi", "libdwarf", "libpg_query", "libssh", "libyang", "lua",
-    #        "llhttp", "md4c", "miniz", "tmux", "p11-kit", "pycryptodome", "quickjs", "utf8proc", "w3m", "zydis"]  # todo: full list of built & fuzzed projects
+    # projects = list(filter(lambda x: x is not None, map(get_proj_info, lines)))
+    projects = [info[0] for info in [get_proj_info(line) for line in lines] if info is not None and info[1] == 'c']
 
     df = pd.concat([get_project_fuzzer_stats(project) for project in projects])
     print(f"In all {len(projects)} built and fuzzed projects:")
