d542f129faf470bcf853e1295a83b1aa473a9c86
Ethan Wang <ethanbwang@gmail.com>
2023-06-06 13:38:13 -0700

    Added regex parsing for projects in built_w_pass.md in analyze.py (#25)
    * Added regex parsing for projects in built_w_pass.md in analyze.py
    
    * feat(analyze): update fuzzer stats to count number of deterministic function
    
    * add explainition for is_deterministic
    
    * update ReportFunctionExecutedPass to newest commit
    
    * use clang frontend to extract source code from C/C++ file
    
    * update ReportPass to master
    
    * fix: rename tests for c
    
    * Changes to regex parsing
    
    * Formatted with black
    
    * update file path fetch and function retrive (#18)
    
    * Added libpng project to built_w_pass.md
    
    * set submodule to master branch
    
    * fix args typo and add a info msg for fuzz_w_pass
    
    * update submodule
    
    Signed-off-by: Peter Rong <PeterRong96@gmail.com>
    
    * demangle
    
    * update submodule
    
    Signed-off-by: Peter Rong <PeterRong96@gmail.com>
    
    * update submodule
    
    Signed-off-by: Peter Rong <PeterRong96@gmail.com>
    
    * fetch source code from docker, search rules need to modified
    
    * update file path retrive and fetch funition method
    
    * update args input, warning log, example output
    
    * when error occurs in fetch code, return null, update env variable
    
    * remove redudent code
    
    * update comment and variables
    
    * able to catch function which defined in three lines
    
    * remove redudent json read
    
    * add comment and recoding
    
    * improve path writting format
    
    * code format improve
    
    * path format improve
    
    * add commit to extract_func_code
    
    * shorten example basicstuff_fuzzer.json
    
    * move demangle.py into func:postprocess
    
    * define file_func_delim
    
    * update requirement.txt, update code_content return value
    
    * update requirement, project post_process
    
    * change return type to None and add a check before writting back to json
    
    * change return type to None
    
    * add write code_content check
    
    * add global const FILE_FUNC_DELIM to common.py
    
    * add requirement
    
    * add requirement
    
    * improve the re to match function
    
    * improve re to match function
    
    * add more path check in get_source_from_docker
    
    * edit re in function matching
    
    * edit re in function matching
    
    * use os.path.splittext, and add TODO
    
    * fetch source code from docker, search rules need to modified
    
    * update file path retrive and fetch funition method
    
    * update args input, warning log, example output
    
    * when error occurs in fetch code, return null, update env variable
    
    * remove redudent code
    
    * update comment and variables
    
    * able to catch function which defined in three lines
    
    * remove redudent json read
    
    * add comment and recoding
    
    * improve path writting format
    
    * code format improve
    
    * path format improve
    
    * add commit to extract_func_code
    
    * shorten example basicstuff_fuzzer.json
    
    * move demangle.py into func:postprocess
    
    * define file_func_delim
    
    * merge clang_get_func_code
    
    * update file retriving
    
    * add comments
    
    ---------
    
    Signed-off-by: Peter Rong <PeterRong96@gmail.com>
    Co-authored-by: Ethan Wang <ebwang@ucdavis.edu>
    Co-authored-by: Ethan He <yfhe@ucdavis.edu>
    Co-authored-by: Peter Rong <PeterRong96@gmail.com>
    
    ---------
    
    Signed-off-by: Peter Rong <PeterRong96@gmail.com>
    Co-authored-by: Ethan He <yfhe@ucdavis.edu>
    Co-authored-by: harrison4anewbe <49333332+harrison4anewbe@users.noreply.github.com>
    Co-authored-by: Peter Rong <PeterRong96@gmail.com>

--- scripts/analyze.py
+++ scripts/analyze.py
@@ -1,6 +1,7 @@
 import sys
 import os
 import pandas as pd
+import re
 
 from scripts.common import *
 from scripts.projects import Project
@@ -14,9 +15,32 @@ def get_project_fuzzer_stats(project: str) -> pd.DataFrame:
     return proj.get_project_stats()
 
 
+def get_proj_info(line: str) -> Optional[Tuple[str, str]]:
+    # pattern = '^[0-9]+\. \[?([A-Za-z0-9\-\_]+)\]?\([A-Za-z\.\_\-/ +]+\),([A-Za-z+]+)'
+    # Group 1 is project name, group 2 is project language
+    # [number]. [project_name]([build_process]),[language]
+    pattern = "^[0-9]+\. \[?([^\]]+)\]?\(.+\),([a-z+]+)"
+
+    line = line.strip()  # remove space at front and end
+    match = re.match(pattern, line)
+
+    if not match:
+        return None
+
+    proj_name = match.group(1)
+    proj_language = match.group(2)
+    return proj_name, proj_language
+
+
 def main():
-    projects = ["clib", "coturn", "croaring", "gpac", "h3", "inchi", "libdwarf", "libpg_query", "libssh", "libyang", "lua",
-            "llhttp", "md4c", "miniz", "tmux", "p11-kit", "pycryptodome", "quickjs", "utf8proc", "w3m", "zydis"]  # todo: full list of built & fuzzed projects
+    with open("./built_w_pass.md", "r") as f:
+        lines = f.readlines()
+    # projects = list(filter(lambda x: x is not None, map(get_proj_info, lines)))
+    projects = [
+        info[0]
+        for info in [get_proj_info(line) for line in lines]
+        if info is not None and info[1] == "c"
+    ]
 
     df = pd.concat([get_project_fuzzer_stats(project) for project in projects])
     print(f"In all {len(projects)} built and fuzzed projects:")
@@ -26,3 +50,4 @@ def main():
 
 if __name__ == "__main__":
     sys.exit(main())
+
