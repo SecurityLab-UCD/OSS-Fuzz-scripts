41dcb24ef21913328fd00a806f8ca386743b439c
harrison4anewbe <49333332+harrison4anewbe@users.noreply.github.com>
2023-06-06 12:35:09 -0700

    update file path fetch and function retrive (#18)
    * Added libpng project to built_w_pass.md
    
    * set submodule to master branch
    
    * fix args typo and add a info msg for fuzz_w_pass
    
    * update submodule
    
    Signed-off-by: Peter Rong <PeterRong96@gmail.com>
    
    * demangle
    
    * update submodule
    
    Signed-off-by: Peter Rong <PeterRong96@gmail.com>
    
    * update submodule
    
    Signed-off-by: Peter Rong <PeterRong96@gmail.com>
    
    * fetch source code from docker, search rules need to modified
    
    * update file path retrive and fetch funition method
    
    * update args input, warning log, example output
    
    * when error occurs in fetch code, return null, update env variable
    
    * remove redudent code
    
    * update comment and variables
    
    * able to catch function which defined in three lines
    
    * remove redudent json read
    
    * add comment and recoding
    
    * improve path writting format
    
    * code format improve
    
    * path format improve
    
    * add commit to extract_func_code
    
    * shorten example basicstuff_fuzzer.json
    
    * move demangle.py into func:postprocess
    
    * define file_func_delim
    
    * update requirement.txt, update code_content return value
    
    * update requirement, project post_process
    
    * change return type to None and add a check before writting back to json
    
    * change return type to None
    
    * add write code_content check
    
    * add global const FILE_FUNC_DELIM to common.py
    
    * add requirement
    
    * add requirement
    
    * improve the re to match function
    
    * improve re to match function
    
    * add more path check in get_source_from_docker
    
    * edit re in function matching
    
    * edit re in function matching
    
    * use os.path.splittext, and add TODO
    
    * fetch source code from docker, search rules need to modified
    
    * update file path retrive and fetch funition method
    
    * update args input, warning log, example output
    
    * when error occurs in fetch code, return null, update env variable
    
    * remove redudent code
    
    * update comment and variables
    
    * able to catch function which defined in three lines
    
    * remove redudent json read
    
    * add comment and recoding
    
    * improve path writting format
    
    * code format improve
    
    * path format improve
    
    * add commit to extract_func_code
    
    * shorten example basicstuff_fuzzer.json
    
    * move demangle.py into func:postprocess
    
    * define file_func_delim
    
    * merge clang_get_func_code
    
    * update file retriving
    
    * add comments
    
    ---------
    
    Signed-off-by: Peter Rong <PeterRong96@gmail.com>
    Co-authored-by: Ethan Wang <ebwang@ucdavis.edu>
    Co-authored-by: Ethan He <yfhe@ucdavis.edu>
    Co-authored-by: Peter Rong <PeterRong96@gmail.com>

--- scripts/demangle.py
+++ scripts/demangle.py
@@ -1,104 +1,156 @@
 import os
 import json
-import glob
 import re
-import ctypes
+import docker
+from logging import error, info, warning
+import cpp_demangle
+import argparse
+from typing import Optional
+from common import *
+from source_code import *
 
-json_path = '../dump/augeas'
-proj_path = '../oss-fuzz/projects/augeas'
-file_names = [f for f in os.listdir(json_path) if f.endswith('.json') and os.path.isfile(json_path+'/'+f)]
-print('Looking for json file: ',file_names)
-def extract_func_code(func_name, file_path):
-    with open(file_path, 'r') as file:
-        code_content = file.read()
-    # init regular expression
-    match_func_init = re.search(".*"+func_name,code_content)
-    if match_func_init:
-        func_init, func_start = match_func_init.group(), match_func_init.end()
-    else:
-        #raise ValueError(f"No match found")
-        print(f"ERROR: No match function '{func_name}' found")
-        return f"ERROR: No match function '{func_name}' found"
-    
-    func_now, open_braces, flag = func_start, 0, 1
 
-    while (open_braces > 0 or flag) and func_now < len(code_content):
-        if code_content[func_now] == "{":
-            open_braces += 1
-            flag=0
-        elif code_content[func_now] == "}":
-            open_braces -= 1
-        func_now += 1
+# get source code from docker
+def get_source_from_docker(
+    file_path: str, file_name: str, proj_name: str
+) -> Optional[str]:
+    # Create a container from the image
+    client = docker.from_env()
+    image = client.images.get(f"gcr.io/oss-fuzz/{proj_name}:latest")
+    container = client.containers.run(image, detach=True)
+    # check file_path, it may not the absolute path
+    if "/" not in file_path or file_path[0] != "/":
+        # Hard coding possible file path to retrive file
+        file_path4 = "/src"
+        file_path3 = os.path.join("/src", file_path)
+        file_path2 = os.path.join("/src", proj_name, "src", file_path)
+        file_path = os.path.join("/src", proj_name, file_path)
 
-    if open_braces > 0: # raise ValueError(f"Malformed function definition for '{func_name}' in code file")
-        print(f"ERROR: Malformed function definition for '{func_name}' in code file")
-        return f"ERROR: Malformed function definition for '{func_name}' in code file"
+        build_sh = ""
+        for chunk in container.get_archive("/src/build.sh")[0]:
+            build_sh += chunk.decode("utf-8")
+        pattern = r"cd\s+(\S+)"
+        matches = re.findall(pattern, build_sh)
+        for match in matches:
+            file_path4 = os.path.join(file_path4, match)
 
-    function_code = code_content[func_start:func_now]
-    return func_init+function_code
+    # Try those possible path
+    try:
+        code_content = ""
 
-def demangle(func: str):
-    # Load the C++ Standard Library
-    libcxx = ctypes.cdll.LoadLibrary("libc++.so.1")
-
-    # Define the signature of the __cxa_demangle() function
-    demangle = libcxx.__cxa_demangle
-    demangle.argtypes = [ctypes.c_char_p, ctypes.c_char_p, ctypes.POINTER(ctypes.c_size_t), ctypes.POINTER(ctypes.c_int)]
-    # Call __cxa_demangle() to demangle the input string
-    output_buffer = ctypes.create_string_buffer(256)
-    output_size = ctypes.c_size_t(256)
-    status = ctypes.c_int(0)
-    result = demangle(func.encode(), output_buffer, ctypes.byref(output_size), ctypes.byref(status))
-    # Forced return demangle value
-    return output_buffer.value.decode('utf-8')
-    # if result == 0:
-    #     # Demangling was successful
-    #     demangled_name = output_buffer.value.decode('utf-8')
-    #     return demangled_name
-    # else:
-    #     # Demangling failed
-    #     print("Demangling failed with status", status)
-
-for json_file_name in file_names:
-    file_name = json_file_name.split('.json')[0]
-    # Find file path
-    if len(glob.glob(proj_path+'/'+file_name+'.c*'))==0:
-        print('ERROR: File '+proj_path+'/'+file_name+'.c*'+" NOT found")
-        continue
-    file_path = glob.glob(proj_path+'/'+file_name+'.c*')[0]
-    # if file_name == 'augeas_api_fuzzer': continue
-    # open Json file and get filename + func name
-    with open(json_path+'/'+json_file_name, 'r') as f:
+        for chunk in container.get_archive(file_path)[0]:
+            code_content += chunk.decode("utf-8")
+    except docker.errors.NotFound:
         try:
-            data = json.load(f)
-        except:
+            code_content = ""
+            for chunk in container.get_archive(file_path2)[0]:
+                code_content += chunk.decode("utf-8")
+        except docker.errors.NotFound:
             try:
-                # the json file start with path, reopen the json file
-                with open(json_path+'/'+json_file_name, 'r') as f:
-                    json_content,flag ='',0
-                    for line in f:
-                        if flag:
-                            json_content+=line
-                        elif line[0]=='{':
-                            json_content+=line
-                            flag=1
-                    data = json.loads(json_content)
-            except:
-                print('ERROR: ',json_file_name, "Json file illegal")
+                code_content = ""
+                for chunk in container.get_archive(file_path3)[0]:
+                    code_content += chunk.decode("utf-8")
+            except docker.errors.NotFound:
+                try:
+                    code_content = ""
+                    for chunk in container.get_archive(file_path4)[0]:
+                        code_content += chunk.decode("utf-8")
+                except docker.errors.NotFound:
+                    code_content = None
+                    error(f"File  {file_path}  NOT found\n")
+    # Stop and remove the container
+    container.stop()
+    container.remove()
+    return code_content
+
+
+def main(proj_name: str):
+    json_path = os.path.join(OSSFUZZ_SCRIPTS_HOME, "dump", proj_name)
+    json_file_names = [f for f in os.listdir(json_path) if f.endswith(".json")]
+    code_list = []
+    info(f"Looking for json file: {json_file_names}")
+
+    for json_file_name in json_file_names:
+        file_name = os.path.splitext(json_file_name)[0]
+        # open Json file and get filename+func_name
+        with open(os.path.join(json_path, json_file_name), "r") as f:
+            try:
+                data = json.load(f)
+            except json.JSONDecodeError:
+                warning(f" {json_file_name}  Json file illegal")
                 continue
-    for file_func_name in data:
-        # if split not matched, need to add a check
-        mangle_func_name = file_func_name.split(file_name)
-        if len(mangle_func_name)<2:
-            raise ValueError(f"ERROR: File '{file_name}' not found, may open wrong json file")
-        else:
-            mangle_func_name = mangle_func_name[1]
-            demangle_func_name = demangle(mangle_func_name)
-            # Add a check demangle is correct?
-            if mangle_func_name == demangle_func_name or demangle_func_name=='':
-                print('ERROR: '+mangle_func_name+' demangle incorrect or unable to demangle')
+        info(f"NUMBER OF FUNCTION: {len(data)}")
+        code_content = ""
+        for cnt in range(len(data)):
+            file_func_name = ""
+            for file_func_name_ in data[cnt]:
+                file_func_name = file_func_name_
+            # if split not matched, need to add a check
+            splited_file_func_name = file_func_name.split(FILE_FUNC_DELIM)
+            file_path, mangle_func_name = (
+                splited_file_func_name[0],
+                splited_file_func_name[1],
+            )
+            # if this is c file then the mangle does not exist
+            try:
+                demangle_func_name = cpp_demangle.demangle(mangle_func_name)
+            except ValueError:
+                demangle_func_name = mangle_func_name
+            if demangle_func_name == None:
+                warning(f" {mangle_func_name} demangle incorrect or unable to demangle")
+                data[cnt][
+                    file_func_name
+                ] = f" {mangle_func_name}demangle incorrect or unable to demangle"
             else:
-                func_name = demangle_func_name.split('(')[0]
-                # print('looking for func ', func_name, ' in  file ', file_path)
-                func_content = extract_func_code(func_name,file_path)
-                print(func_content)
+                func_name = demangle_func_name.split("(")[0]
+                # Try to get code_content when the source code file haven't been retrived yet
+                # TODO: featch all code_content in one time
+                info(f"Open source file: {file_path}")
+                output_path = os.path.join(
+                    OSSFUZZ_SCRIPTS_HOME, "output", proj_name, "codes"
+                )
+                output_code_path = os.path.join(
+                    output_path, os.path.basename(file_path)
+                )
+                # if current file have not been retrived, open docker to get it
+                if file_path not in code_list:
+                    code_content = get_source_from_docker(
+                        file_path, file_name, proj_name
+                    )
+                    if not os.path.exists(output_path):
+                        os.makedirs(output_path)
+                    if code_content == None:
+                        error(f"{file_path} CODE content ERROR")
+                        continue
+                    code_list.append(file_path)
+                    with open(output_code_path, "w") as fi:
+                        # write to .c file
+                        fi.write(code_content)
+                # get function content
+                func_content = clang_get_func_code(output_code_path, func_name)
+                # write to json
+                data[cnt][file_func_name] = {
+                    "code": func_content,
+                    "data": data[cnt][file_func_name],
+                }
+        # write back to JSON
+        with open(
+            os.path.join(OSSFUZZ_SCRIPTS_HOME, "output", proj_name, json_file_name), "w"
+        ) as json_file:
+            # write to JSON file
+            json.dump(data, json_file)
+
+
+# main function
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Catch source code")
+    parser.add_argument(
+        "-n",
+        "--name",
+        type=str,
+        required=False,
+        default="coturn",
+        help="The project name to fetch",
+    )
+    args = parser.parse_args()
+    main(args.name)
