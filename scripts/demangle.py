0156221c81b012760ca5c1c8f07e5295436d1451
harrison4anewbe <49333332+harrison4anewbe@users.noreply.github.com>
2023-06-07 20:25:26 -0700

    feat(postprocess): get all files from docker at once (#28)
    * get all files in one time
    
    * update clang_get_source_code
    
    * update requeirement
    
    * add function doc-string

--- scripts/demangle.py
+++ scripts/demangle.py
@@ -2,6 +2,8 @@ import os
 import json
 import re
 import docker
+import tarfile
+import io
 from logging import error, info, warning
 import cpp_demangle
 import argparse
@@ -10,62 +12,75 @@ from common import *
 from source_code import *
 
 
-# get source code from docker
-def get_source_from_docker(
-    file_path: str, file_name: str, proj_name: str
-) -> Optional[str]:
-    # Create a container from the image
+# Get all files from docker
+def copy_files_from_docker(proj_name: str, output_path: str) -> bool:
+    """
+    Get all target project files from docker
+
+    Args:
+        proj_name (str): Target project name.
+        output_path (str): The local path to store the files.
+
+    Returns:
+        bool: True if the copying was successful, False otherwise.
+    """
     client = docker.from_env()
     image = client.images.get(f"gcr.io/oss-fuzz/{proj_name}:latest")
     container = client.containers.run(image, detach=True)
-    # check file_path, it may not the absolute path
-    if "/" not in file_path or file_path[0] != "/":
-        # Hard coding possible file path to retrive file
-        file_path4 = "/src"
-        file_path3 = os.path.join("/src", file_path)
-        file_path2 = os.path.join("/src", proj_name, "src", file_path)
-        file_path = os.path.join("/src", proj_name, file_path)
-
-        build_sh = ""
-        for chunk in container.get_archive("/src/build.sh")[0]:
-            build_sh += chunk.decode("utf-8")
-        pattern = r"cd\s+(\S+)"
-        matches = re.findall(pattern, build_sh)
-        for match in matches:
-            file_path4 = os.path.join(file_path4, match)
-
-    # Try those possible path
     try:
-        code_content = ""
-
-        for chunk in container.get_archive(file_path)[0]:
-            code_content += chunk.decode("utf-8")
+        stream, _ = container.get_archive("/src")
     except docker.errors.NotFound:
-        try:
-            code_content = ""
-            for chunk in container.get_archive(file_path2)[0]:
-                code_content += chunk.decode("utf-8")
-        except docker.errors.NotFound:
-            try:
-                code_content = ""
-                for chunk in container.get_archive(file_path3)[0]:
-                    code_content += chunk.decode("utf-8")
-            except docker.errors.NotFound:
-                try:
-                    code_content = ""
-                    for chunk in container.get_archive(file_path4)[0]:
-                        code_content += chunk.decode("utf-8")
-                except docker.errors.NotFound:
-                    code_content = None
-                    error(f"File  {file_path}  NOT found\n")
-    # Stop and remove the container
+        error(f"Docker  {proj_name} path /src  NOT found\n")
+        return False
+
+    fileobj = io.BytesIO()
+    for chunk in stream:
+        fileobj.write(chunk)
+    fileobj.seek(0)
+
+    # Extract files from the file-like object
+    with tarfile.open(fileobj=fileobj) as tar:
+        tar.extractall(output_path)
+
     container.stop()
     container.remove()
-    return code_content
+    return True
+
+
+# get source code from local
+def get_source_code_path(suffix_file_path: str, output_path: str) -> Optional[str]:
+    """
+    Match and get target file path from local
+
+    Args:
+        suffix_file_path (str): Suffix path of target file.
+        output_path (str): The path of project.
+
+    Returns:
+        str: target source code file path.
+    """
+    # Transliteration special characterss, replace special characters with \ ahead of themselves
+    pattern = r"[\[\].,{}()\W_]"
+    suffix_file_path = re.sub(pattern, r"\\\g<0>", suffix_file_path)
+
+    pattern = f".*{suffix_file_path}$"
+    regex = re.compile(pattern)
+
+    for root, directories, files in os.walk(output_path):
+        for file in files:
+            file_path = os.path.join(root, file)
+            if regex.match(file_path):
+                return file_path
+    return None
 
 
 def main(proj_name: str):
     json_path = os.path.join(OSSFUZZ_SCRIPTS_HOME, "dump", proj_name)
+    output_path = os.path.join(OSSFUZZ_SCRIPTS_HOME, "output", proj_name, "codes")
+    # Get all files from docker
+    if not copy_files_from_docker(proj_name, output_path):
+        error(f"Fetch {proj_name} source code failed")
+        return
     json_file_names = [f for f in os.listdir(json_path) if f.endswith(".json")]
     code_list = []
     info(f"Looking for json file: {json_file_names}")
@@ -103,31 +118,17 @@ def main(proj_name: str):
                 ] = f" {mangle_func_name}demangle incorrect or unable to demangle"
             else:
                 func_name = demangle_func_name.split("(")[0]
-                # Try to get code_content when the source code file haven't been retrived yet
-                # TODO: featch all code_content in one time
-                info(f"Open source file: {file_path}")
-                output_path = os.path.join(
-                    OSSFUZZ_SCRIPTS_HOME, "output", proj_name, "codes"
-                )
-                output_code_path = os.path.join(
-                    output_path, os.path.basename(file_path)
-                )
-                # if current file have not been retrived, open docker to get it
-                if file_path not in code_list:
-                    code_content = get_source_from_docker(
-                        file_path, file_name, proj_name
-                    )
-                    if not os.path.exists(output_path):
-                        os.makedirs(output_path)
-                    if code_content == None:
-                        error(f"{file_path} CODE content ERROR")
-                        continue
-                    code_list.append(file_path)
-                    with open(output_code_path, "w") as fi:
-                        # write to .c file
-                        fi.write(code_content)
+                # Get code path from local
+                code_path = get_source_code_path(file_path, output_path)
+                if code_path == None:
+                    warning(f"Get source code path error {file_path}")
+                    data[cnt][file_func_name] = {
+                        "code": None,
+                        "data": data[cnt][file_func_name],
+                    }
+                    continue
                 # get function content
-                func_content = clang_get_func_code(output_code_path, func_name)
+                func_content = clang_get_func_code(code_path, func_name)
                 # write to json
                 data[cnt][file_func_name] = {
                     "code": func_content,
