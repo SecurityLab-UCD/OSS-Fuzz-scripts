7078e3d46382870b3043465ca1dace92832741ac
Ethan Wang <ebwang@ucdavis.edu>
2023-07-22 20:16:17 -0700

    Made requested changes and fixes to built_w_pass.md

--- scripts/cpp_dump.py
+++ scripts/cpp_dump.py
@@ -1,11 +1,15 @@
-from os import path
 import os
 import argparse
+import logging
 
-from analyze import get_proj_info
-from get_fuzz_dump import get_fuzz_dump, directories
+from scripts.analyze import get_proj_info
+from scripts.get_fuzz_dump import get_fuzz_dump
+from scripts.projects import Project
 
-def build_and_fuzz(option: str) -> None: 
+
+# Option can be a for building and fuzzing all projects or c for continuing from the last built project
+# If continuing, will skip projects that already have a dump directory with json files inside
+def build_and_fuzz(option: str) -> None:
     # Get all C++ projects
     with open("./built_w_pass.md", "r") as f:
         lines = f.readlines()
@@ -22,23 +26,22 @@ def build_and_fuzz(option: str) -> None:
     for project in projects:
         dump_dir = os.path.join("./dump", project)
         # If continue, check if dump directory has json files already
-        if option == 'c' and os.path.exists(dump_dir) and len(os.listdir(dump_dir)) > 0:
+        if option == "c" and os.path.exists(dump_dir) and len(os.listdir(dump_dir)) > 0:
             continue
         else:
             # Delete previous dump if want to build and fuzz all projects
-            if option == 'a':
-                os.system("rm -rf {}".format(dump_dir))
+            if option == "a":
+                os.system(f"rm -rf {dump_dir}")
 
             # Build and fuzz project
-            os.system(
-                "python3 ./scripts/projects.py -d {} -p auto_build_w_pass --cpp=c++".format(
-                    project
-                )
-            )
-            print("Done building {}".format(project))
+            # os.system(
+            #     f"python3 ./scripts/projects.py -d {project} -p auto_build_w_pass --cpp=c++"
+            # )
+            dataset = Project(project, "fuzz", "dump")
+            dataset.auto_build_w_pass(cpp="c++")
+            print(f"Done building {project}")
             get_fuzz_dump(project)
-            print("Done fuzzing {}".format(project))
-
+            print(f"Done fuzzing {project}")
 
 
 def main():
@@ -50,13 +53,17 @@ def main():
     option = args.option.lower()
 
     # Run build and fuzz
-    if option == 'a' or option == "all":
-        build_and_fuzz('a')
-    elif option == 'c' or option == "continue":
-        build_and_fuzz('c')
+    if option == "a" or option == "all":
+        build_and_fuzz("a")
+    elif option == "c" or option == "continue":
+        build_and_fuzz("c")
     else:
         print("Options: a(ll), c(ontinue)")
         exit(1)
 
+
 if __name__ == "__main__":
+    logging.basicConfig()
+    logging.getLogger().setLevel(logging.INFO)
     main()
+
--- scripts/get_fuzz_dump.py
+++ scripts/get_fuzz_dump.py
@@ -1,20 +1,12 @@
-from os import path
 import os
 import argparse
+import logging
 
-def directories(project: str) -> None:
-    build_dir = os.path.join("./oss-fuzz/build/out", project)
-    dump_dir = os.path.join("./dump", project)
-    fuzz_dir = os.path.join("./fuzz", project)
+from scripts.projects import Project
 
-    print("Build Directory: {}".format(build_dir))
-    print(os.listdir(build_dir))
-    print("Dump Directory: {}".format(dump_dir))
-    print(os.listdir(dump_dir))
-    print("Fuzz Directory: {}".format(fuzz_dir))
-    print(os.listdir(fuzz_dir))
 
-def get_fuzz_dump(project: str) -> None: 
+def get_fuzz_dump(project: str, max_fuzz_time: int = 1800) -> None:
+    dataset = Project(project, "fuzz", "dump")
     time = 45
     build_dir = os.path.join("./oss-fuzz/build/out", project)
     dump_dir = os.path.join("./dump", project)
@@ -23,78 +15,105 @@ def get_fuzz_dump(project: str) -> None:
     targets = [
         f
         for f in os.listdir(build_dir)
-        if not os.path.isdir(os.path.join(build_dir, f)) and f != "llvm-symbolizer" and '.' not in f
+        if not os.path.isdir(os.path.join(build_dir, f))
+        and f != "llvm-symbolizer"
+        and "." not in f
     ]
     total_binaries = len(targets)
 
     lowest_time_fail = None
     longest_time_success = 0
 
-    os.system("rm -f " + dump_dir + "/*")
-    os.system("rm -rf " + fuzz_dir + "/timeout*")
-    os.system("rm -rf " + build_dir + "/timeout*")
-    
+    # Remove files that will be replaced when fuzzing
+    os.system(f"rm -f {dump_dir}/*")
+    os.system(f"rm -rf {fuzz_dir}/timeout*")
+    os.system(f"rm -rf {fuzz_dir}/crashes/*")
+    os.system(f"rm -rf {build_dir}/timeout*")
+
     while True:
         # Fuzz target
-        os.system(
-            "python3 ./scripts/projects.py -d {} -p fuzz_w_pass -ft {}s".format(
-                project, time
-            )
-        )
+        # os.system(
+        #     "python3 ./scripts/projects.py -d {} -p fuzz_w_pass -ft {}s".format(
+        #         project, time
+        #     )
+        # )
+        dataset.fuzz(jobs=int(os.environ.get("CORES")), fuzztime=time, dump=True)
 
         # Count number of json files
         num_files_dumped = 0
         for path in os.listdir(dump_dir):
             if path[len(path) - 5 : len(path)] == ".json":
                 num_files_dumped += 1
-        print("Dumped: {} out of {} files".format(num_files_dumped, total_binaries))
-        # Max fuzz time of 1800 seconds for now
-        if longest_time_success >= 1800:
-            break
-        elif num_files_dumped < total_binaries:
+        print(f"Dumped: {num_files_dumped} out of {total_binaries} files")
+
+        if num_files_dumped < total_binaries:
             lowest_time_fail = time
             new_time = int((lowest_time_fail + longest_time_success) / 2)
-            if new_time <= longest_time_success or lowest_time_fail and new_time >= lowest_time_fail:
+            if (
+                new_time <= longest_time_success
+                or lowest_time_fail
+                and new_time >= lowest_time_fail
+            ):
                 break
             time = new_time
         else:
             longest_time_success = time
-            new_time = int((lowest_time_fail + longest_time_success) / 2) if lowest_time_fail is not None else longest_time_success * 2 
-            if lowest_time_fail and new_time >= lowest_time_fail or new_time <= longest_time_success:
+            new_time = (
+                int((lowest_time_fail + longest_time_success) / 2)
+                if lowest_time_fail is not None
+                else longest_time_success * 2
+            )
+            if (
+                lowest_time_fail
+                and new_time >= lowest_time_fail
+                or new_time <= longest_time_success
+            ):
                 break
             time = new_time
 
-        # Remove dump files and timeout files
-        os.system("rm -f " + dump_dir + "/*")
-        os.system("rm -rf " + fuzz_dir + "/timeout*")
-        os.system("rm -rf " + build_dir + "/timeout*")
-        
-        # Max fuzz time of 1800 seconds for now
-        if time >= 1800:
+        # Remove files that will be replaced when fuzzing again
+        os.system(f"rm -f {dump_dir}/*")
+        os.system(f"rm -rf {fuzz_dir}/timeout*")
+        os.system(f"rm -rf {fuzz_dir}/crashes/*")
+        os.system(f"rm -rf {build_dir}/timeout*")
+
+        if time >= max_fuzz_time or time < 1:
             break
 
-    # Fuzz again with the longest time that still dumps all of the json files
-    os.system("rm -f " + dump_dir + "/*")
-    os.system("rm -rf " + fuzz_dir + "/timeout*")
-    os.system("rm -rf " + build_dir + "/timeout*")
-    os.system(
-        "python3 ./scripts/projects.py -d {} -p fuzz_w_pass -ft {}s".format(
-            project, longest_time_success
+    # Remove files that will be replaced when fuzzing again
+    os.system(f"rm -f {dump_dir}/*")
+    os.system(f"rm -rf {fuzz_dir}/timeout*")
+    os.system(f"rm -rf {fuzz_dir}/crashes/*")
+    os.system(f"rm -rf {build_dir}/timeout*")
+    # os.system(
+    #     "python3 ./scripts/projects.py -d {} -p fuzz_w_pass -ft {}s".format(
+    #         project, longest_time_success
+    #     )
+    # )
+    if longest_time_success > 0:
+        # Fuzz again with the longest time that still dumps all of the json files
+        dataset.fuzz(
+            jobs=int(os.environ.get("CORES")), fuzztime=longest_time_success, dump=True
         )
-    )
-    print("Done fuzzing {} for {} seconds.".format(project, longest_time_success))
+
+        print(f"Done fuzzing {project} for {longest_time_success} seconds")
+    else:
+        print(f"{project} fuzzing cannot dump for every binary, fuzzing for one second")
+        # Fuzz again for one second
+        dataset.fuzz(jobs=int(os.environ.get("CORES")), fuzztime=1, dump=True)
 
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument("dataset", type=str, help="The dataset to fuzz")
+    parser.add_argument("project", type=str, help="The project to fuzz")
 
     args = parser.parse_args()
 
-    project = args.dataset
-
-    get_fuzz_dump(project)
+    get_fuzz_dump(args.project)
 
 
 if __name__ == "__main__":
+    logging.basicConfig()
+    logging.getLogger().setLevel(logging.INFO)
     main()
+
