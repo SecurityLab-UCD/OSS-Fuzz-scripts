0e7a9e4a39e44116fd82d9abf7240c6cd8623872
Ethan Wang <ebwang@ucdavis.edu>
2023-07-27 18:18:35 -0700

    New tests for mangle_example.cpp

--- tests/mangle_example.cpp
+++ tests/mangle_example.cpp
@@ -2,6 +2,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+class Parent {
+  public:
+    void foo() {
+        printf("Parent Foo");
+    }
+};
+
+class Child : public Parent {
+  public:
+    void foo() {
+        printf("Child Foo");
+    }
+};
+
+template <typename T> T t_foo(T x, T y) {
+    return (x > y) ? x : y;
+}
+
 int foo(int x) { return x + 1; }
 
 int foo(int x, int y) { return x + y; }
@@ -24,6 +42,15 @@ int main() {
 
   double d = boo(1.0);
   printf("d = %f\n", d);
+
+  printf("%d\n", t_foo<int>(5, 7));
+  printf("%f\n", t_foo<double>(5.5, 7.25));
+
+  Parent p = Parent();
+  p.foo();
+
+  Child c = Child();
+  c.foo();
 
   return 0;
 }
