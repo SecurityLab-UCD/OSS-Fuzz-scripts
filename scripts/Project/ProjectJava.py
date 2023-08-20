117359a4e108bdc04a052c067b4b246f3047e678
Yifeng He <yfhe@ucdavis.edu>
2023-08-20 13:51:45 +0800

    add build_w_pass for java

--- scripts/Project/ProjectJava.py
+++ scripts/Project/ProjectJava.py
@@ -9,7 +9,31 @@ from scripts.demangle import main as main_post_process
 
 class ProjectJava(Project):
     def build_w_pass(self, build_script: str = "build_w_pass.sh"):
-        raise NotImplementedError
+        dockerfile = f"{OSSFUZZ}/projects/{self.project}/Dockerfile"
+        os.system(f"cp {dockerfile} {dockerfile}.bak")
+        agent = "java-io-capture-1.0-SNAPSHOT.jar"
+        shutil.copyfile(
+            f"{OSSFUZZ_SCRIPTS_HOME}/java-io-capture/target/{agent}",
+            f"{OSSFUZZ}/projects/{self.project}",
+        )
+        report_agent_config = (
+            f"ENV REPORT_AGENT=$SRC/{self.project}/{agent}\n"
+            f"COPY {build_script} $SRC/build.sh\n"
+            f"COPY {agent} $REPORT_AGENT\n"
+        )
+
+        with open(dockerfile, "a") as f:
+            f.write("".join(report_agent_config))
+
+        # backup previous built fuzzers
+        build_dir = f"{OSSFUZZ}/build/out/{self.project}"
+        if path.isdir(build_dir):
+            os.system(f"mv {build_dir} {build_dir}_bak")
+
+        self.build()
+
+        os.system(f"mv {dockerfile}.bak {dockerfile}")
+        shutil.rmtree(f"{OSSFUZZ}/projects/{self.project}/{agent}")
 
     def auto_build_w_pass(self, cpp: str):
         raise NotImplementedError
