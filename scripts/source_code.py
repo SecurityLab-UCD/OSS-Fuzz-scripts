18225ed7b408305a4dddf9f62a98c3a8363e7e46
Yifeng He <yfhe@ucdavis.edu>
2023-08-30 21:40:10 +0800

    return (type, var_name) list instead of just var_name

--- scripts/source_code.py
+++ scripts/source_code.py
@@ -185,26 +185,26 @@ def py_get_imported_modules(code: str) -> List[str]:
     return module_names
 
 
-def c_get_params(code: str) -> Optional[list[str]]:
+def c_get_params(code: str) -> Optional[list[tuple[str, str]]]:
     """get parameters of a C function
 
     Args:
         code (str): source code of the function
 
     Returns:
-        Optional[list[str]]: list of parameters, None if `code` is not a function
+        Optional[list[str]]: list of (type, name) parameter pairs, None if `code` is not a function
     """
 
     def walk_tree_for_params(node):
         if node.kind == clang.cindex.CursorKind.FUNCTION_DECL:  # type: ignore
-            function_name = node.spelling
-            parameters = [param.spelling for param in node.get_arguments()]
-
+            parameters = [
+                (param.type.spelling, param.spelling) for param in node.get_arguments()
+            ]
             return parameters
 
         for child in node.get_children():
             result = walk_tree_for_params(child)
-            if result:
+            if result is not None:
                 return result
 
         return None
--- tests/test_c_get_params.py
+++ tests/test_c_get_params.py
@@ -8,13 +8,18 @@ class TestGetParamsC(unittest.TestCase):
         code = "int add(int x, int y) { return x + y; }"
         params = c_get_params(code)
 
-        self.assertEqual(params, ["int x", "int y"])
+        self.assertEqual(params, [("int", "x"), ("int", "y")])
 
-    def test_c_multi_line(self):
-        pass
+    def test_c_no_param(self):
+        code = "void do_nothing() { return; }"
+        params = c_get_params(code)
 
-    def test_c_multiple_curly_brackets(self):
-        pass
+        self.assertEqual(params, [])
+
+    def test_c_not_func_decl(self):
+        code = "int x = 5;"
+        params = c_get_params(code)
+        self.assertIsNone(params)
 
 
 if __name__ == "__main__":
