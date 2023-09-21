f376f7068bee65b5cb5fbf41196391d43af77641
Ethan He <yfhe@ucdavis.edu>
2023-09-21 23:20:29 +0000

    add unittest generation for python

--- scripts/unittest_gen.py
+++ scripts/unittest_gen.py
@@ -30,9 +30,39 @@ def to_cpp_GoogleTest(func_name: str, io_pairs: list[list[str]]) -> str:
     return "\n".join(lines)
 
 
+def to_py_unittest(func_name: str, io_pairs: list[list[str]]) -> str:
+    match func_name.split("."):
+        case [function_name]:
+            decl = f"def test_{func_name}():"
+        case [class_name, function_name]:
+            decl = f"def test_{class_name}_{function_name}():"
+        case _:
+            decl = f"def test_{func_name}():"
+
+    lines = [decl]
+    for io in io_pairs:
+        inputs = io[0]
+        outputss = io[1]
+        real = f"{func_name}({', '.join(inputs)})"
+
+        # todo: consider optional parameters
+        # idx 0 is the actual rnt value, the rest are pointer inputs
+        expected_outputs = [outputs[0] for outputs in outputss if len(outputs) > 0]
+        if all(expected_outputs[0] == output for output in expected_outputs):
+            # all expected outputs are the same
+            expected = expected_outputs[0]
+            lines.append(f"    assert {real} == {expected}")
+        else:
+            # todo: decide how to handle different expected outputs
+            pass
+
+    return "\n".join(lines)
+
+
 UNITTEST_GENERATOR = {
     "c": to_cpp_GoogleTest,
     "cpp": to_cpp_GoogleTest,
     "cxx": to_cpp_GoogleTest,
     "cc": to_cpp_GoogleTest,
+    "python": to_py_unittest,
 }
